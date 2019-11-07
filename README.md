6a3cd14f8825fde4e3cd21690542efc88c54d806
olaniyi <ajayiolaniyi8@gmail.com>
2019-11-07 23:59:43 +0100

    delete sport folder

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # locallibrary
 This is Django application
 This is a local library applications
+I delete some folder
--- sport/teams/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Club, Player
-
-# Register your models here.
-
-admin.site.register(Club)
-admin.site.register(Player)
-
-
